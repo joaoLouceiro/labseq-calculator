def45b79d82e74714be206f21ae51352e5b5d820
JLouceiro <jrlouceiro@live.com>
2026-04-10 15:45:22 +0100

    Improved version of the cache. - Had to drop the pre-warming of the cache, as it was filling up the memory pretty quickly (BigIntegers use up a lot of it). - Ended up changing the algorithm altogether

--- src/main/java/com/jlouceiro/LabseqService.java
+++ src/main/java/com/jlouceiro/LabseqService.java
@@ -1,57 +1,49 @@
 package com.jlouceiro;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import io.quarkus.cache.Cache;
 import io.quarkus.cache.CacheName;
 import io.quarkus.cache.CaffeineCache;
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class LabseqService {
 
-    @Inject
     @CacheName("libseq-cache")
     Cache cache;
-
-    @PostConstruct
-    public void initialize() {
-        cache.as(CaffeineCache.class).put(0, CompletableFuture.completedFuture(BigInteger.ZERO));
-        cache.as(CaffeineCache.class).put(1, CompletableFuture.completedFuture(BigInteger.ONE));
-        cache.as(CaffeineCache.class).put(2, CompletableFuture.completedFuture(BigInteger.ZERO));
-        cache.as(CaffeineCache.class).put(3, CompletableFuture.completedFuture(BigInteger.ONE));
-    }
 
     public BigInteger getLabseqResult(int n) {
         if (n < 0) {
             throw new LabseqException("Input must be a non-negative integer.");
         }
 
-        for (int i = 4; i <= n; i++) {
-            CompletableFuture<BigInteger> futureA = cache.as(CaffeineCache.class).getIfPresent(i - 4);
-            CompletableFuture<BigInteger> futureB = cache.as(CaffeineCache.class).getIfPresent(i - 3);
+        return cache.get(n, k -> {
+            if (n == 0 || n == 2) {
+                return BigInteger.ZERO;
+            }
 
-            BigInteger sum = futureA.resultNow().add(futureB.resultNow());
+            if (n == 1 || n == 3) {
+                return BigInteger.ONE;
+            }
 
-            cache.as(CaffeineCache.class).put(i, CompletableFuture.completedFuture(sum));
-        }
+            BigInteger prev4 = BigInteger.ZERO;
+            BigInteger prev3 = BigInteger.ONE;
+            BigInteger prev2 = BigInteger.ZERO;
+            BigInteger prev1 = BigInteger.ONE;
+            BigInteger current = prev4;
 
-        return getIfPresent(n);
+            for (int i = 4; i <= n; i++) {
+                current = prev4.add(prev3);
+                prev4 = prev3;
+                prev3 = prev2;
+                prev2 = prev1;
+                prev1 = current;
+            }
+            return current;
+        }).await().indefinitely();
 
     }
 
-    private BigInteger getIfPresent(Object key) {
-        CompletableFuture<BigInteger> future = cache.as(CaffeineCache.class).getIfPresent(key);
-        return future.resultNow();
-    }
-
-    private void putInCache(int key, BigInteger value) {
-        cache.as(CaffeineCache.class).put(key, CompletableFuture.completedFuture(BigInteger.ZERO));
-    }
 }
